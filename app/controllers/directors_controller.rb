678718ff1ba0c616404807bbaf13336a375717ba
sahilagr123 <sahilagr@uchicago.edu>
2024-11-11 21:39:41 +0000

    e

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -50,6 +50,39 @@ class DirectorsController < ApplicationController
   end
 
   def eldest
+
+    dir = Director.where.not({ :dob => nil})
+    
+    @eldest_age = 0
+    @eldest = dir.at(0)
+
+    year_now = Time.now.year
+    month_now = Time.now.month
+    day_now = Time.now.day
+
+    #@youngest = dir.at(0)
+    age = 0
+    dir.all.each do |a_director|
+      #calculate age
+      age = year_now - a_director.dob.year
+      if (month_now > a_director.dob.month)
+        age = age + 1
+      elsif (month_now = a_director.dob.month)
+        if (day_now >= a_director.dob.day)
+          age = age + 1
+        end
+      end
+
+      #if age < youngest_age, then current director is youngest. 
+      if (age > @eldest_age)
+        @eldest_age = age
+        @eldest = a_director
+      end
+
+      age = 0
+    
+    end
+
     render({ :template => "director_templates/eldest"})
   end
 end
