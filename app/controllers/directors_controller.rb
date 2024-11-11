0b0751607900e822ba3cbd29f0d25474fe149c85
sahilagr123 <sahilagr@uchicago.edu>
2024-11-11 02:31:59 +0000

    b

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,21 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list"})
   end
+
+  def show
+    the_id = params.fetch("the_id")
+
+    @matching_records = Director.where({ :id => the_id })
+
+    @the_director = @matching_records.at(0)
+    render({ :template => "director_templates/details"})
+  end
+
+  def youngest
+    render({ :template => "director_templates/youngest"})
+  end
+
+  def eldest
+    render({ :template => "director_templates/eldest"})
+  end
 end
